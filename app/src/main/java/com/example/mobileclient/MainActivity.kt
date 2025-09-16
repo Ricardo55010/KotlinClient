459c5cb94d021139a2a485596f34f6b231b5f805
Ricardo55010 <richi_mari.2011@hotmail.com>
2025-09-16 16:59:07 -0600

    edit: adding wrapper to composable Surfeace function which changes the app color

--- app/src/main/java/com/example/mobileclient/MainActivity.kt
+++ app/src/main/java/com/example/mobileclient/MainActivity.kt
@@ -7,12 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobileclient.ui.theme.MobileClientTheme
-
+import androidx.compose.ui.graphics.Color
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,13 +33,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    Surface(color = Color.Yellow) {
+        Text(
         text = "Hello Dev $name!",
         modifier = modifier
     )
+    }
 }
 
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MobileClientTheme {
